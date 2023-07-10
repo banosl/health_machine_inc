79698550a7a8581c458728ab336f95e16e2fff4a
Leo Banos Garcia <bsleo625@gmail.com>
2023-07-10 14:58:20 -0600

    adds #has_fever? to Temperature and test for a False result

--- lib/temperature.rb
+++ lib/temperature.rb
@@ -1,3 +1,13 @@
 class Temperature
-  
+  attr_reader :celsius, :farenheit, :kelvin
+
+  def initialize
+    @celsius = 37.00
+    @farenheit = 98.6
+    @kelvin = 310.15
+  end
+
+  def has_fever?
+    @farenheit >= 100.4
+  end
 end
--- spec/temperature_spec.rb
+++ spec/temperature_spec.rb
@@ -1,5 +1,34 @@
 require './lib/temperature.rb'
+require './lib/patient.rb'
 
 RSpec.describe Temperature do
-  
+  describe 'Patient' do  
+    it 'patient has default temperatures in celsius, farenheit, and kelvin' do
+      patient_1 = Patient.new("James", 43)
+
+      expect(patient_1.temperature.celsius).to eq(37)
+      expect(patient_1.temperature.farenheit).to eq(98.6)
+      expect(patient_1.temperature.kelvin).to eq(310.15)
+    end
+  end
+
+  describe 'measure_temperature' do
+
+  end
+
+  describe 'has_fever?' do
+    before :each do
+      @patient_1 = Patient.new("James", 43)
+      @patient_2 = Patient.new("Sandra", 22)
+    end
+
+    xit 'can return TRUE for when a patient has a temperature of over 100.4 F, 38 C, 311.15 K' do
+      expect(@patient_1.temperature.has_fever?).to eq(true)
+    end
+
+    it 'can return FALSE for when a patient has a temperature below 100.4 F, 38 C, 311.15 K' do
+      expect(@patient_2.temperature.farenheit).to eq(98.6)
+      expect(@patient_2.temperature.has_fever?).to eq(false)
+    end
+  end
 end
