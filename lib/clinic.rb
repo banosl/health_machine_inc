2015e7c1f84138fb16c72a9eced6af046a4fcfe9
Leo Banos Garcia <bsleo625@gmail.com>
2023-07-10 18:22:14 -0600

    Adds #patient_latest_temperatures to Clinic

--- lib/clinic.rb
+++ lib/clinic.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'date'
 require './lib/patient.rb'
 
 class Clinic
@@ -18,6 +19,18 @@ class Clinic
 
     @patients.select do |patient|
       patient.name.include?(name)
-    end
+    end.first
+  end
+
+  def patient_latest_temperatures(name)
+    patient = find_patient_by_name(name)
+    record_1 = patient.latest_temperature_readings.to_a[0]
+    record_2 = patient.latest_temperature_readings.to_a[1]
+    record_3 = patient.latest_temperature_readings.to_a[2]
+
+    "Name\t Age\t Temperature Reading\t\s\s Date/Time\t\t Fever\n
+#{patient.name}\t #{patient.age}\t #{record_1[1][0]} F, #{record_1[1][1]} C, #{record_1[1][2]} K\s #{record_1[0].strftime("%a %b %e %H:%M")}\t #{patient.temperature[0].has_fever?}
+#{patient.name}\t #{patient.age}\t #{record_1[1][0]} F, #{record_1[1][1]} C, #{record_1[1][2]} K\s #{record_2[0].strftime("%a %b %e %H:%M")}\t #{patient.temperature[1].has_fever?}
+#{patient.name}\t #{patient.age}\t #{record_1[1][0]} F, #{record_1[1][1]} C, #{record_1[1][2]} K\s #{record_3[0].strftime("%a %b %e %H:%M")}\t #{patient.temperature[2].has_fever?}"
   end
 end
--- spec/clinic_spec.rb
+++ spec/clinic_spec.rb
@@ -29,21 +29,31 @@ RSpec.describe Clinic do
       clinic_1.new_patient("Mia", 21)
       patient = clinic_1.find_patient_by_name("mia")
 
-      expect(patient[0].name).to eq("Mia")
-      expect(patient[0].age).to eq(21)
+      expect(patient.name).to eq("Mia")
+      expect(patient.age).to eq(21)
     end
   end
 
-  #note, add an ID attribute to each patient for searching
+  #note, add an ID attribute to each patient for searching so that there are no duplicate patients pulled with the same name.
 
-  # describe 'patient latest temperatures' do
-  #   it 'can return the temp records for a patient in a neat table' do
-  #     clinic_1 = Clinic.new("GroupHealth")
+  describe 'patient latest temperatures' do
+    it 'can return the temp records for a patient in a neat table' do
+      clinic_1 = Clinic.new("GroupHealth")
       
-  #     clinic_1.new_patient("Mia", 21)
+      clinic_1.new_patient("Mia", 21)
+      patient = clinic_1.find_patient_by_name("mia")
+      patient.record_temperature("F", 99)
+      patient.record_temperature("F", 98)
+      patient.record_temperature("F", 100)
 
-  #     expect(clinic_1.patient_latest_temperatures("Mia")).to eq("Name\t Age\t Temperature Reading 1\t Timte\t Temperature Reading\t Fever\n
-  #                                                               Mia\t 21\t ")
-  #   end
-  # end
+      record_1 = patient.latest_temperature_readings.to_a[0]
+      record_2 = patient.latest_temperature_readings.to_a[1]
+      record_3 = patient.latest_temperature_readings.to_a[2]
+
+      expect(clinic_1.patient_latest_temperatures("Mia")).to eq("Name\t Age\t Temperature Reading\t\s\s Date/Time\t\t Fever\n
+#{patient.name}\t #{patient.age}\t #{record_1[1][0]} F, #{record_1[1][1]} C, #{record_1[1][2]} K\s #{record_1[0].strftime("%a %b %e %H:%M")}\t #{patient.temperature[0].has_fever?}
+#{patient.name}\t #{patient.age}\t #{record_1[1][0]} F, #{record_1[1][1]} C, #{record_1[1][2]} K\s #{record_2[0].strftime("%a %b %e %H:%M")}\t #{patient.temperature[1].has_fever?}
+#{patient.name}\t #{patient.age}\t #{record_1[1][0]} F, #{record_1[1][1]} C, #{record_1[1][2]} K\s #{record_3[0].strftime("%a %b %e %H:%M")}\t #{patient.temperature[2].has_fever?}")
+    end
+  end
 end
